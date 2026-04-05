0cc66311a9b8e2039cbbe363cc7b89592b119035
Gideon Adeti <gideonadeti0@gmail.com>
2026-04-05 14:11:16 +0000

    feat: increase Gunicorn worker count to 8 for enhanced application performance

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,4 @@
 set -eu
 cd /app
 python scripts/create_tables.py
-exec gunicorn --bind 0.0.0.0:8000 --workers 4 --timeout 120 wsgi:app
+exec gunicorn --bind 0.0.0.0:8000 --workers 8 --timeout 120 wsgi:app
